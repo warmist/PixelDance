39bfcbc6ad56657b96c260396f5df5dfb4982b87
warmist <warmist@gmail.com>
2018-12-06 16:06:44 +0200

    Some more playaround.

--- projects/ecology.lua
+++ projects/ecology.lua
@@ -6,6 +6,7 @@ local size=STATE.size
 
 local oversample=0.5
 is_remade=false
+
 function update_img_buf(  )
 	local nw=math.floor(size[1]*oversample)
 	local nh=math.floor(size[2]*oversample)
@@ -21,6 +22,9 @@ end
 update_img_buf()
 config=make_config({
 	{"color",{0.4,0.4,0.3,0.1},type="color"},
+	{"zoom",1,type="float",min=1,max=10},
+	{"t_x",0,type="float",min=-1,max=1},
+	{"t_y",0,type="float",min=-1,max=1},
 	},config)
 local draw_shader=shaders.Make[==[
 #version 330
@@ -32,8 +36,12 @@ uniform vec2 rez;
 uniform vec4 sun_color;
 uniform sampler2D tex_main;
 uniform sampler2D tex_sun;
+uniform float zoom;
+uniform vec2 translate;
+
 void main(){
 	vec2 normed=(pos.xy+vec2(1,1))/2;
+	normed=normed/zoom+translate;
 	float hsun=texture(tex_sun,vec2(normed.x,0)).x;
 	float v=1;
 	//v=1-smoothstep(normed.y,hsun-0.005,1);
@@ -66,7 +74,7 @@ local directions4={
 local pixel_types={ --alpha used to id types
 	sand={124,100,80,255},
 	wall={20,80,100,100},
-	dead_plant={50,20,30,254},
+	dead_plant={50,20,30,190},
 	plant_seed={10,150,50,48},
 	worm_body={255,100,80,52},
 }
@@ -76,7 +84,7 @@ function pixel_init(  )
 	local cx = math.floor(w/2)
 	local cy = math.floor(h/2)
 
-	for i=1,5000 do
+	for i=1,w*h*0.2 do
 		local x=math.random(0,w-1)
 		local y=math.random(0,h-1)
 		img_buf:set(x,y,pixel_types.sand)
@@ -102,6 +110,7 @@ function pixel_init(  )
 		end
 		
 	end
+	--[[ h wall
 	local wall_size = 8
 	for i=1,5 do
 		local x=math.random(0,w-1)
@@ -109,8 +118,8 @@ function pixel_init(  )
 		for i=0,wall_size do
 			img_buf:set(x,y+i,pixel_types.wall)
 		end
-		
 	end
+	]]
 end
 if is_remade then
 pixel_init()
@@ -139,6 +148,24 @@ function pixel_step(  )
 						local d=img_buf:get(tx,y-1)
 						if d.a==0 then
 							img_buf:set(tx,y-1,c)
+							img_buf:set(x,y,{0,0,0,0})
+						end
+					end
+				end
+			elseif c.a>180 then --> 180 liquidy
+				local d=img_buf:get(x,y-1)
+				if d.a==0 then
+					img_buf:set(x,y-1,c)
+					img_buf:set(x,y,{0,0,0,0})
+				else
+					local tx=x+1
+					if math.random()>0.5 then
+						tx=x-1
+					end
+					if tx>=0 and tx<=w-1 then
+						local d=img_buf:get(tx,y)
+						if d.a==0 then
+							img_buf:set(tx,y,c)
 							img_buf:set(x,y,{0,0,0,0})
 						end
 					end
@@ -185,6 +212,10 @@ function is_sunlit( x,y )
 end
 function plant_step()
 	--super config
+	--growth stuff
+	local chance_up=0.6
+	local chance_sunlit=0.9
+	--costs and food
 	local sun_gain=1
 	local grow_cost_const=1
 	local grow_cost_buffer=1.2
@@ -242,10 +273,11 @@ function plant_step()
 			local grow_cost=grow_cost_const+(#v.path)*0.05--+math.max(ty*2-25,0)
 			if ty<h-1 and (food_balance>grow_cost*grow_cost_buffer or #v.path<3) then
 
-				if math.random()>0.2 then
+				if math.random()>chance_up then
+					-- prefer sunlit directions
 					local right=is_sunlit(tx+1,ty)
 					local left=is_sunlit(tx-1,ty)
-					if not(left== right) then
+					if math.random()<chance_sunlit and not(left== right) then
 						if left then
 							tx=tx-1
 						else
@@ -309,17 +341,18 @@ end
 worms=worms or {}
 if is_remade then worms={} end
 
-function add_worm(  )
+function add_worm( x,y )
 	local w=img_buf.w
 	local h=img_buf.h
-	local x=math.random(0,w-1)
-	local y=0
+	x=x or math.random(0,w-1)
+	y=y or 0
 	table.insert(worms,{pixel_types.worm_body,food=500,dead=false,tail={{x,y}}})
 	img_buf:set(x,y,pixel_types.worm_body)
 end
 
 
 function worm_step( )
+	local surface_bias=0.08
 	local grow_cost_const=500
 	local grow_cost_buffer=2
 	local max_food=20000
@@ -343,7 +376,9 @@ function worm_step( )
 		end
 		--movement logic
 		local d=directions4[math.random(1,#directions4)]
-
+		if math.random()<surface_bias then
+			d={0,1}
+		end
 		local tx=d[1]+x
 		local ty=d[2]+y
 		if #v.tail>1 then
@@ -435,29 +470,46 @@ function update()
 	__clear()
 	imgui.Begin("ecology")
 	draw_config(config)
-	imgui.End()
-	if is_mouse_down() then
-		add_worm()
+	if imgui.Button("Kill plants") then
+		for i,v in ipairs(plants) do
+			v.dead=true
+		end
 	end
-	if math.random()>0.9 then
+	imgui.SameLine()
+	if imgui.Button("Reset world") then
+		img_buf=nil
+		update_img_buf()
+	end
+	imgui.End()
+	local md,x,y=is_mouse_down(  )
+	if md then
+		local tx,ty=math.floor(x*oversample),math.floor(img_buf.h-y*oversample)
+		if tx<0 then tx=0 end
+		if ty<0 then ty=0 end
+		add_worm(tx,ty)
+		
+	end
+	if math.random()>0.8 then
 		add_plant()
 	end
-	if math.random()>0.99 and #worms<10 then
+	if math.random()>0.99 and #worms<50 then
 		add_worm()
 	end
  	pixel_step( )
  	plant_step()
  	worm_step()
 	draw_shader:use()
-	tex_pixel:use(0)
+	tex_pixel:use(0,0,1)
 
 	--tex_pixel.t:set(size[1]*oversample,size[2]*oversample,3)
 	img_buf:write_texture(tex_pixel)
-	tex_sun:use(1)
+	tex_sun:use(1,0,1)
 	sun_buffer:write_texture(tex_sun)
 
 	draw_shader:set_i("tex_main",0)
 	draw_shader:set_i("tex_sun",1)
+	draw_shader:set("zoom",config.zoom)
+	draw_shader:set("translate",config.t_x,config.t_y)
 	draw_shader:set("sun_color",config.color[1],config.color[2],config.color[3],config.color[4])
 	draw_shader:draw_quad()
 end
