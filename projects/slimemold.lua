c950e325f3fd2dac54ae3343108d4bd1752789f9
warmist <warmist@gmail.com>
2019-02-27 14:38:34 +0200

    slime mold

---
+++ projects/slimemold.lua
@@ -0,0 +1,11 @@
+--from: https://sagejenson.com/physarum
+--[[
+	ideas:
+		add other types of agents
+		more interactive "world"
+			* eatible food
+			* sand-like sim?
+			* more senses (negative?)
+--]]
+
+
