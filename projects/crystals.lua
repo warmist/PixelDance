f940b0b15d69f2a03621eb8c160e498001198736
warmist <warmist@gmail.com>
2019-04-15 16:30:32 +0300

    Diffuse now works

--- projects/crystals.lua
+++ projects/crystals.lua
@@ -1,5 +1,5 @@
 --[[
-	* mass transfer and crystalization
+	* mass transfer and crystallization
 	* block diffusion by other crystals
 --]]
 require "common"
@@ -8,23 +8,28 @@ local win_h=768
 
 __set_window_size(win_w,win_h)
 local oversample=0.5
-local map_w=(win_w*oversample)
-local map_h=(win_h*oversample)
+local map_w=math.floor(win_w*oversample)
+local map_h=math.floor(win_h*oversample)
 
 local size=STATE.size
 
 img_buf=img_buf or make_image_buffer(map_w,map_h)
-material=material or make_flt_buffer(map_w,map_h)
+material=material or make_float_buffer(map_w,map_h)
 function resize( w,h )
 	img_buf=make_image_buffer(map_w,map_h)
-	material=make_flt_buffer(map_w,map_h)
+	material=make_float_buffer(map_w,map_h)
 end
 
 local size=STATE.size
 
 tick=tick or 0
 config=make_config({
+
+	{"diffuse_steps",1,min=0,max=10,type="int"},
+	{"diffuse",0.5,type="float"},
+	{"decay",0.99,type="float"},
 	{"simulate",true,type="boolean"},
+	{"add_mat",true,type="boolean"},
 },config)
 image_no=image_no or 0
 
@@ -61,30 +66,43 @@ void main(){
 	color=vec4(r,0,0,1);
 }
 ]==]
-function diffuse_and_decay( tex,tex_out,w,h,diffuse,decay )
+function diffuse_and_decay( tex,tex_out,w,h,diffuse,decay,steps )
+	steps=steps or 1
 	--[[
 	TODO:
-		* add step count
 		* add mask to block mass transfer
 	--]]
 	decay_diffuse_shader:use()
-    tex:use(0)
-    --tex_pixel.t:set(size[1]*oversample,size[2]*oversample,3)
-    decay_diffuse_shader:set_i("tex_main",0)
-    decay_diffuse_shader:set("decay",decay)
-    decay_diffuse_shader:set("diffuse",diffuse)
-    if not tex_out:render_to(w,h) then
-		error("failed to set framebuffer up")
+	for i=1,steps do
+	    tex:use(0)
+	    decay_diffuse_shader:set_i("tex_main",0)
+	    decay_diffuse_shader:set("decay",decay)
+	    decay_diffuse_shader:set("diffuse",diffuse)
+	    if not tex_out:render_to(w,h) then
+			error("failed to set framebuffer up")
+		end
+	    decay_diffuse_shader:draw_quad()
+	    --swap textures
+	    local c = tex
+    	tex=tex_out
+    	tex_out=c
 	end
-    decay_diffuse_shader:draw_quad()
     __render_to_window()
+    return tex_out
 end
 
 
 local need_save
-local visit_tex = textures.Make()
-last_pos=last_pos or {0,0}
-function save_img(tile_count)
+local mat_tex1 = textures.Make()
+local mat_tex2 = textures.Make()
+function write_mat()
+	mat_tex1:use(0)
+	material:write_texture(mat_tex1)
+	mat_tex2:use(0)
+	material:write_texture(mat_tex2)
+end
+write_mat()
+function save_img()
 	local config_serial=__get_source().."\n--AUTO SAVED CONFIG:\n"
 	for k,v in pairs(config) do
 		if type(v)~="table" then
@@ -94,99 +112,64 @@ function save_img(tile_count)
 	img_buf:save(string.format("saved_%d.png",os.time(os.date("!*t"))),config_serial)
 	image_no=image_no+1
 end
-function draw_visits(  )
-	local lmax=0
-	local lmin=math.huge
-	local vst=visits
+local draw_shader=shaders.Make(
+[==[
+#version 330
+#line 118
+out vec4 color;
+in vec3 pos;
 
-	for x=0,size[1]-1 do
-	for y=0,size[2]-1 do
-		local vp=vst:get(x,y)
-		local v=vp.r*vp.r+vp.g*vp.g+vp.b*vp.b
-		if lmax<v then lmax=v end
-		if lmin>v then lmin=v end
-	end
-	end
-	lmax=math.log(math.sqrt(lmax)+1)
-	lmin=math.log(math.sqrt(lmin)+1)
-	log_shader:use()
-	visit_tex:use(0)
-	visits:write_texture(visit_tex)
-	log_shader:set("min_max",lmin,lmax)
-	log_shader:set_i("tex_main",0)
-	local auto_scale=0
-	if config.auto_scale_color then auto_scale=1 end
-	log_shader:set_i("auto_scale_color",auto_scale)
-	log_shader:draw_quad()
-	if need_save then
-		save_img(tile_count)
-		need_save=nil
-	end
-end
+uniform sampler2D tex_main;
 
-
-function blend_rgb( c1,c2,t )
-	local ret={}
-	for i=1,4 do
-		ret[i]=(c2[i]-c1[i])*t+c1[i]
-	end
-	return ret
-end
-function clear_screen(full )
-	local s=STATE.size
-	if full then
-		visits:clear()
-	else
-		for i=0,visits.w*visits.h-1 do
-			visits.d[i].a=0
-		end
-	end
-	local cc=config.prev_color
-	local ss=math.floor(config.seed_size/2)
-	for i=-ss,ss do
-		for j=-ss,ss do
-		local c=visits:get(math.floor(s[1]/2)+i,math.floor(s[2]/2)+j)
-		c.r=c.r+cc[1]
-		c.g=c.g+cc[2]
-		c.b=c.b+cc[3]
-		c.a=1
-	end
-	end
-	
-	restart_count=0
-	counter=0
-	rand:seed(42)
+void main(){
+    vec2 normed=(pos.xy+vec2(1,1))/2;
+    vec4 pixel=texture(tex_main,normed);
+    color=vec4(pixel.x,0,0,1);
+}
+]==])
+function draw(  )
+	draw_shader:use()
+    mat_tex1:use(0)
+	draw_shader:set_i("tex_main",0)
+	draw_shader:draw_quad()
 end
 function update(  )
 	__no_redraw()
 	__clear()
-	imgui.Begin("Hello")
+	imgui.Begin("Crystals")
 	local s=STATE.size
 	draw_config(config)
-	
+
 	if imgui.Button("Clear image") then
-		print("Clearing:"..s[1].."x"..s[2])
-		clear_screen(true)
-	end
-	imgui.SameLine()
-	if imgui.Button("Save image") then
-		img_buf:save("saved_"..image_no..".png","Saved by PixelDance")
-		need_save=true
+		print(material.w,material.h)
+		--clear_screen(true)
+		for j=0,map_h-1 do
+			for i=0,map_w-1 do
+				material:set(i,j,0)
+			end
+		end
+		for i=0,map_w-1 do
+			material:set(i,math.floor(map_h/2),1000)
+		end
+		write_mat()
 	end
 	imgui.End()
-	local color_dt
-	config.color,color_dt=step_color_hsl(config.color,config.next_color,config.color_step)
-
-	for i=1,config.ppframe do
-		config.color=blend_rgb(config.prev_color,config.next_color,(i-1)/config.ppframe)
-		local c=flt_pixel(config.color)
-		rand_ray(rand,rand_off, c)
+	if config.simulate then
+		if config.add_mat then
+			mat_tex1:use(0)
+			material:read_texture(mat_tex1)
+			for i=0,map_w-1 do
+				material:set(i,math.floor(map_h/2),1000)
+			end
+			material:write_texture(mat_tex1)
+		end
+		diffuse_and_decay(mat_tex1,mat_tex2,map_w,map_h,config.diffuse,config.decay,config.diffuse_steps)
+		if(config.diffuse_steps%2==1) then
+			local c=mat_tex1
+			mat_tex1=mat_tex2
+			mat_tex2=c
+	    end
 	end
-	restart_count=restart_count+1
+	draw()
 
-	if restart_count>config.restart then
-		clear_screen(false)
-	end
-	draw_visits()
-	
 end
