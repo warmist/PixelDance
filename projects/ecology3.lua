dc82dfd8ba8d26030f82719538663b5a8f6168e1
Warmist <warmist@gmail.com>
2019-05-26 12:43:49 +0300

    ecology3: sand now works(!)

--- projects/ecology3.lua
+++ projects/ecology3.lua
@@ -19,7 +19,7 @@ local win_w=768
 local win_h=768
 
 __set_window_size(win_w,win_h)
-local oversample=0.125
+local oversample=0.25
 
 local map_w=math.floor(win_w*oversample)
 local map_h=math.floor(win_h*oversample)
@@ -30,7 +30,7 @@ local size=STATE.size
 
 is_remade=false
 local max_particle_count=win_w*win_h
-current_particle_count= 500
+current_particle_count= 5000
 function update_buffers()
 	if particles_pos==nil or particles_pos.w~=max_particle_count then
 		particles_pos=make_flt_half_buffer(max_particle_count,1)
@@ -97,16 +97,19 @@ uniform vec2 res;
 uniform vec2 zoom;
 uniform vec2 translate;
 void main(){
-    gl_PointSize=pix_size*zoom.y;
+    gl_PointSize=int(pix_size*abs(zoom.y));
     vec2 pix_int_pos=floor(position.xy+vec2(0.5,0.5))+vec2(0.5,0.5);
     vec2 pix_pos=(pix_int_pos/res-vec2(0.5,0.5))*vec2(2,-2);
     gl_Position.xy=pix_pos;
     gl_Position.xy=(gl_Position.xy*zoom+translate*vec2(2,-2));
     gl_Position.zw=vec2(0,1.0);//position.z;
     pos=gl_Position.xyz;
-    if (particle_type==1)
+    
+
+    
+    if (particle_type==1u)
         col=vec4(0,0,1,0.8);
-    else if(particle_type==0)
+    else if(particle_type==0u)
         col=vec4(0,0,0,0);
     else
     {
@@ -131,11 +134,11 @@ int_count=0
 intersect_list={}
 function resolve_intersects(  )
     for k,v in pairs(intersect_list) do
+        --local num_close=#v todo: maybe some crowding prevention?
         for i,v in ipairs(v) do
             local t=particle_types:get(v[1],0)
-
-            if t==1 then
-                local water_move_speed=0.01
+            if t==1 then --sand
+                local sand_move_speed=0.05
                 local p=particles_pos:get(v[1],0)
                 local s=particles_speeds:get(v[1],0)
                 --target pos
@@ -145,29 +148,29 @@ function resolve_intersects(  )
                 p.r=v[2]
                 p.g=v[3]
 
-                local ly=ty
+                local ly=ty+1
                 local x1=tx-1
                 if x1<0 then x1=map_w-1 end
                 local x2=tx+1
                 if x2>=map_w then x2=0 end
 
-                if ly<map_h-1 then
+                if ly<=map_h-1 then
                     local sl=scratch_layer:get(tx,ly).a
                     if sl>0 then
                         local s1=scratch_layer:get(x1,ly).a
                         local s2=scratch_layer:get(x2,ly).a
                         if s1==0 then
-                            s.r=s.r-water_move_speed
+                            s.r=s.r-sand_move_speed
                         elseif s2==0 then
-                            s.r=s.r+water_move_speed
+                            s.r=s.r+sand_move_speed
                         else
                             local ss1=static_layer:get(tx,ly).a
                             local ss2=static_layer:get(x1,ly).a
                             local ss3=static_layer:get(x2,ly).a
-                            print(tx,ly,ss1,ss2,ss3)
-                            --static_layer:set(tx,ty,{255,255,255,255})
-                            --particle_types:set(v[1],0,0)
-                            
+                            if ss1>0 and ss2>0 and ss3>0 then
+                                static_layer:set(tx,ty,{255,255,255,255})
+                                particle_types:set(v[1],0,0)
+                            end
                         end
                     end
                 end
@@ -188,7 +191,7 @@ function resolve_intersects(  )
                     particle_types:set(v[1],0,0)
                 end
             end
-            
+
         end
     end
     
@@ -271,18 +274,18 @@ function scratch_update(  )
     --draw_shader:draw_quad()
 
     place_pixels_shader:use()
-    --[[tex_scratch:use(1,0,1)
+
+    -- [[
+    tex_scratch:use(1,0,1)
     if not tex_scratch:render_to(scratch_layer.w,scratch_layer.h) then
         error("failed to set framebuffer up")
-    end]]
+    end
+    --]]
     place_pixels_shader:set_i("pix_size",1)
     place_pixels_shader:set("res",map_w,map_h)
     place_pixels_shader:set("zoom",1*map_aspect_ratio,-1)
     place_pixels_shader:set("translate",0,0)
-    if need_clear then
-        __clear()
-        need_clear=false
-    end
+
     place_pixels_shader:push_iattribute(particle_types.d,"particle_type",1,GL_UNSIGNED_BYTE)
     place_pixels_shader:draw_points(particles_pos.d,current_particle_count)
     __render_to_window()
@@ -372,43 +375,37 @@ function update()
     imgui.End()
     __render_to_window()
 
+    
+    update_buffers()
+
+    draw_shader:use()
     if config.draw then
-        update_buffers()
-
-        draw_shader:use()
         tex_pixel:use(0,0,1)
+        static_layer:write_texture(tex_pixel)
+    else
         tex_scratch:use(0,0,1)
+    end
 
-        
-        --static_layer:write_texture(tex_pixel)
+    draw_shader:set_i("tex_main",0)
+    draw_shader:set_i("res",map_w,map_h)
+    draw_shader:set("zoom",config.zoom*map_aspect_ratio,config.zoom)
+    draw_shader:set("translate",config.t_x,config.t_y)
+    draw_shader:draw_quad()
 
-        draw_shader:set_i("tex_main",0)
-        draw_shader:set_i("res",map_w,map_h)
-        draw_shader:set("zoom",config.zoom*map_aspect_ratio,config.zoom)
-        draw_shader:set("translate",config.t_x,config.t_y)
-        draw_shader:draw_quad()
-
-        --[==[
+    if config.draw then
     	place_pixels_shader:use()
         place_pixels_shader:set_i("pix_size",math.floor(1/oversample))
         place_pixels_shader:set("res",map_w,map_h)
         place_pixels_shader:set("zoom",config.zoom*map_aspect_ratio,config.zoom)
         place_pixels_shader:set("translate",config.t_x,config.t_y)
-        --[[if not tex_pixel:render_to(img_buf.w,img_buf.h) then
-            error("failed to set framebuffer up")
-        end]]
         if need_clear then
             __clear()
             need_clear=false
         end
         place_pixels_shader:push_iattribute(particle_types.d,"particle_type",1,GL_UNSIGNED_BYTE)
         place_pixels_shader:draw_points(particles_pos.d,current_particle_count)
-        --[[__render_to_window()
-        ]]
-        --]==]
-        
-        
     end
+    
 
     if need_save then
         save_img()
