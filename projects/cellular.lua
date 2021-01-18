47371e76976e98b4fb4a000e1f65d04b0f160543
Warmist <warmist@gmail.com>
2021-01-18 20:10:09 +0200

    CA: ideas

---
+++ projects/cellular.lua
@@ -0,0 +1,9 @@
+--[===[
+some ideas:
+	* voronoi centers with 1d CA outwards
+	* CA with "rule switches"
+		- on borders?
+		- on smooth noise
+		- on some value (e.g. region alive count >0.5)
+	* more complicated N state CA
+--]===]
