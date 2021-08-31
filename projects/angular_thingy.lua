9ca0a926f9fa4de9dbff82ecb608dc16b15f96de
warmist <warmist@gmail.com>
2021-08-31 11:19:46 +0300

    AT: minor changes

--- projects/angular_thingy.lua
+++ projects/angular_thingy.lua
@@ -53,8 +53,7 @@ config=make_config({
     {"particle_opacity",0.01,type="floatsci",min=0,max=1,power=10},
     {"particle_reset_iter",100,type="int",min=0,max=1000},
     {"particle_wait_iter",0,type="int",min=0,max=1000},
-    {"gamma",1,type="floatsci",min=0.5,max=2,power=0.5},
-    {"level",1,type="floatsci",min=0.5,max=2000,power=10},
+    {"gamma",1,type="floatsci",min=-2,max=2,power=0.5},
     },config)
 
 
@@ -348,6 +347,82 @@ in vec3 pos;
 #define M_PI   3.14159265358979323846264338327950288
 uniform sampler2D tex_rotation;
 uniform sampler2D tex_speeds;
+uniform float time_input;
+
+//  Simplex 3D Noise 
+//  by Ian McEwan, Ashima Arts
+//
+vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
+
+float snoise(vec3 v){ 
+  const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+
+// First corner
+  vec3 i  = floor(v + dot(v, C.yyy) );
+  vec3 x0 =   v - i + dot(i, C.xxx) ;
+
+// Other corners
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min( g.xyz, l.zxy );
+  vec3 i2 = max( g.xyz, l.zxy );
+
+  //  x0 = x0 - 0. + 0.0 * C 
+  vec3 x1 = x0 - i1 + 1.0 * C.xxx;
+  vec3 x2 = x0 - i2 + 2.0 * C.xxx;
+  vec3 x3 = x0 - 1. + 3.0 * C.xxx;
+
+// Permutations
+  i = mod(i, 289.0 ); 
+  vec4 p = permute( permute( permute( 
+             i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
+           + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+
+// Gradients
+// ( N*N points uniformly over a square, mapped onto an octahedron.)
+  float n_ = 1.0/7.0; // N=7
+  vec3  ns = n_ * D.wyz - D.xzx;
+
+  vec4 j = p - 49.0 * floor(p * ns.z *ns.z);  //  mod(p,N*N)
+
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+
+  vec4 x = x_ *ns.x + ns.yyyy;
+  vec4 y = y_ *ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+
+  vec4 b0 = vec4( x.xy, y.xy );
+  vec4 b1 = vec4( x.zw, y.zw );
+
+  vec4 s0 = floor(b0)*2.0 + 1.0;
+  vec4 s1 = floor(b1)*2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+
+  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+
+  vec3 p0 = vec3(a0.xy,h.x);
+  vec3 p1 = vec3(a0.zw,h.y);
+  vec3 p2 = vec3(a1.xy,h.z);
+  vec3 p3 = vec3(a1.zw,h.w);
+
+//Normalise gradients
+  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+
+// Mix final noise value
+  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  m = m * m;
+  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
+                                dot(p2,x2), dot(p3,x3) ) );
+}
 
 #define SC_SAMPLE(dx,dy,w) \
     {\
@@ -607,7 +682,12 @@ void main(){
         rotation.xyz=euler_from_q(normalize(cnt));
 #elif 1
     vec4 org=q_from_euler(rotation.xyz);
-    org=qmul(org,q_from_euler(speeds.xyz*dt));
+    vec3 speed_out;
+    float time=time_input;
+    speed_out.x=snoise(vec3(normed,time*speeds.x));
+    speed_out.y=snoise(vec3(normed,time*speeds.y));
+    speed_out.z=snoise(vec3(normed,time*speeds.z));
+    org=qmul(org,q_from_euler(speed_out*0.05*dt));
     vec4 avg=quat_avg_at_pos(normed);
     vec4 cnt=q_slerp(org,avg,0.5);
     //vec4 cnt=quat_input_rotated(quat_avg_at_pos(normed),speeds*dt);
@@ -808,6 +888,11 @@ function reset_agent_data()
     local chance_move=0.8
     local chance_no_move=0.5
     for i=0,agent_count-1 do
+        --local z=math.random()
+        local z=rand_gaussian(0.1,0.5)
+        if z>1 then z=1 end
+        if z<0 then z=0 end
+
         if math.random()> chance_move then
             -- [[
             local x=math.random()*2-1
@@ -818,8 +903,9 @@ function reset_agent_data()
             if y>1 then y=y-2 end
             if x<-1 then x=x+2 end
             if y<-1 then y=y+2 end
+
             agent_color:set(i,0,{x*0.5+0.5,y*0.5+0.5,(math.abs(x+y))*0.5,0.0001})
-            agent_state:set(i,0,{x,y,math.random(),0})
+            agent_state:set(i,0,{x,y,z,0})
         else
             local max_r=(1/map_w)*(map_w)
 
@@ -841,7 +927,7 @@ function reset_agent_data()
             if y>1 then y=y-2 end
             if x<-1 then x=x+2 end
             if y<-1 then y=y+2 end
-            agent_state:set(i,0,{x,y,math.random(),0})
+            agent_state:set(i,0,{x,y,z,0})
         end
     end
     for i=1,agent_state_buffer.count do
@@ -878,6 +964,7 @@ function save_img(  )
     img_buf_save:read_frame()
     img_buf_save:save(string.format("saved_%d.png",os.time(os.date("!*t"))),config_serial)
 end
+global_time=global_time or 0
 function sim_tick(  )
     update_rotations_shader:use()
     local t1=vector_buffer:get()
@@ -887,7 +974,7 @@ function sim_tick(  )
     speed_buffer:get():use(3)
     update_rotations_shader:set_i("tex_rotation",1);
     update_rotations_shader:set_i("tex_speeds",3);
-
+    update_rotations_shader:set("time_input",global_time)
     if not t_out:render_to(vector_layer.w,vector_layer.h) then
         error("failed to set framebuffer up")
     end
@@ -895,6 +982,7 @@ function sim_tick(  )
     __render_to_window()
 
     vector_buffer:advance()
+    global_time=global_time+0.001
 end
 function agent_tick(  )
     agent_shader:use()
@@ -951,10 +1039,8 @@ function find_min_max( tex,buf )
         --local lum=math.abs(v.g)
         --local lum=math.abs(v.g)+math.abs(v.r)+math.abs(v.b)
         local lum=math.abs(v.g)
-        if lum > config.level then
-            avg_lum=avg_lum+math.log(1+lum)
-            count=count+1
-        end
+        avg_lum=avg_lum+math.log(1+lum)
+        count=count+1
     end
     end
     avg_lum = math.exp(avg_lum / count);
