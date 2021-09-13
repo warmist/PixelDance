e97a0460f6489a481c6e889bf4071ddbba8c8b1d
Warmist <warmist@gmail.com>
2021-09-13 20:51:54 +0300

    AT: Start of particle transport

--- projects/angular_thingy.lua
+++ projects/angular_thingy.lua
@@ -688,13 +688,9 @@ void main(){
     vec4 org=q_from_euler(rotation.xyz);
     vec3 speed_out;
     float time=time_input;
-#if 0
-    speed_out.x=snoise(vec3(normed+vec2(0.124,2.33),time*speeds.x+0.157));
-    speed_out.y=snoise(vec3(normed+vec2(4.05,4.783),time*speeds.y-0.7777));
-    speed_out.z=snoise(vec3(normed+vec2(11.28,7.11),time*speeds.z+1.579));
-#else
+
     speed_out=speeds.xyz;
-#endif
+
     org=qmul(org,q_from_euler(speed_out*0.05*dt));
     vec4 avg=quat_avg_at_pos(normed);
     vec4 cnt=q_slerp(org,avg,0.5);
@@ -716,100 +712,28 @@ void main(){
 agent_shader=shaders.Make(
 [==[
 #version 330
+#line 715
 
 layout(location = 0) in vec4 position;
 
 out vec4 point_out;
-
+out vec2 pos_old;
 #define M_PI 3.1415926535897932384626433832795
 
 uniform sampler2D tex_angles;
 uniform float speed;
 uniform float time_input;
 
-//  Simplex 3D Noise 
-//  by Ian McEwan, Ashima Arts
-//
-vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
-vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
-
-float snoise(vec3 v){ 
-  const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
-  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
-
-// First corner
-  vec3 i  = floor(v + dot(v, C.yyy) );
-  vec3 x0 =   v - i + dot(i, C.xxx) ;
-
-// Other corners
-  vec3 g = step(x0.yzx, x0.xyz);
-  vec3 l = 1.0 - g;
-  vec3 i1 = min( g.xyz, l.zxy );
-  vec3 i2 = max( g.xyz, l.zxy );
-
-  //  x0 = x0 - 0. + 0.0 * C 
-  vec3 x1 = x0 - i1 + 1.0 * C.xxx;
-  vec3 x2 = x0 - i2 + 2.0 * C.xxx;
-  vec3 x3 = x0 - 1. + 3.0 * C.xxx;
-
-// Permutations
-  i = mod(i, 289.0 ); 
-  vec4 p = permute( permute( permute( 
-             i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
-           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
-           + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
-
-// Gradients
-// ( N*N points uniformly over a square, mapped onto an octahedron.)
-  float n_ = 1.0/7.0; // N=7
-  vec3  ns = n_ * D.wyz - D.xzx;
-
-  vec4 j = p - 49.0 * floor(p * ns.z *ns.z);  //  mod(p,N*N)
-
-  vec4 x_ = floor(j * ns.z);
-  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
-
-  vec4 x = x_ *ns.x + ns.yyyy;
-  vec4 y = y_ *ns.x + ns.yyyy;
-  vec4 h = 1.0 - abs(x) - abs(y);
-
-  vec4 b0 = vec4( x.xy, y.xy );
-  vec4 b1 = vec4( x.zw, y.zw );
-
-  vec4 s0 = floor(b0)*2.0 + 1.0;
-  vec4 s1 = floor(b1)*2.0 + 1.0;
-  vec4 sh = -step(h, vec4(0.0));
-
-  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
-  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
-
-  vec3 p0 = vec3(a0.xy,h.x);
-  vec3 p1 = vec3(a0.zw,h.y);
-  vec3 p2 = vec3(a1.xy,h.z);
-  vec3 p3 = vec3(a1.zw,h.w);
-
-//Normalise gradients
-  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
-  p0 *= norm.x;
-  p1 *= norm.y;
-  p2 *= norm.z;
-  p3 *= norm.w;
-
-// Mix final noise value
-  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
-  m = m * m;
-  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
-                                dot(p2,x2), dot(p3,x3) ) );
-}
-
 void main()
 {
     vec2 normed=(position.xy+vec2(1,1))*vec2(0.5,0.5);
+    pos_old=normed;
     //TODO: this bilinear/nn iterpolates. Does this make sense?
     vec3 angle=texture(tex_angles,normed).xyz;
     vec2 delta;
 
-    float noise=snoise(vec3(normed,time_input*0.05))*0.5+0.5;
+    //float noise=snoise(vec3(normed,time_input*0.05))*0.5+0.5;
+    float noise=position.z;
 
 #if 1
     if(noise>0.6666)
@@ -848,12 +772,26 @@ void main()
     point_out=vec4(p,pout,position.z);
 }
 ]==],
-[==[ void main(){} ]==],"point_out"
+[==[
+#version 410
+#line 777
+
+uniform sampler2D tex_angles;
+in vec4 point_out;
+in vec2 pos_old;
+
+out vec4 col;
+
+void main(){
+    
+
+}
+]==],"point_out"
 )
 agent_draw_shader=shaders.Make(
 [==[
 #version 410
-
+#line 791
 layout(location = 0) in vec4 position;
 layout(location = 1) in vec4 particle_color;
 uniform float norm_wave;
@@ -1057,6 +995,7 @@ void main()
 ]==],
 [==[
 #version 410
+#line 995
 
 in vec4 col;
 
