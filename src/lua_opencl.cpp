67aeb21288928b004cd903b5c3fc24580f2d4f39
warmist <warmist@gmail.com>
2023-02-08 10:30:27 +0200

    some more opencl stuff

--- src/lua_opencl.cpp
+++ src/lua_opencl.cpp
@@ -7,6 +7,39 @@
 #include "lualib.h"
 #include "lauxlib.h"
 static cl_device_id device;
+static cl_context context;
+
+int lua_build_program(lua_State* L)
+{
+    program=clCreateProgramWithSource(context, 1, source, source_size, &err);
+    if (err < 0)
+    {
+
+    }
+    err=clBuildProgram(program, 0, nullptr, nullptr, nullptr, nullptr);
+    if (err < 0)
+    {
+
+    }
+    kernel = clCreateKernel(program, KERNEL_FUNC, &err);
+
+    //add set kernel arg
+
+}
+int lua_create_buffer(lua_State* L)
+{
+    //clEnqueueWriteBuffer
+    //clEnqueueReadBuffer
+    //https://registry.khronos.org/OpenCL/sdk/2.2/docs/man/html/clCreateFromGLTexture.html
+
+}
+static const luaL_Reg lua_opencl_lib[] = {
+    { "build_program",lua_build_program},
+    { "create_buffer",lua_create_buffer},
+
+    { NULL, NULL }
+};
+
 int lua_open_opencl(lua_State* L)
 {
     if (!cl_lite_init())
@@ -29,7 +62,10 @@ int lua_open_opencl(lua_State* L)
     }*/
     if (err < 0) {
         luaL_error(L,"Couldn't access any devices");
-        
     }
+
+    luaL_newlib(L, lua_opencl_lib);
+    lua_setglobal(L, "opencl");
+
     return 0;
 }
