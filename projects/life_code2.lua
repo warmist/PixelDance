1561f2477d08e3cdd1bd2f887bcd1e6b685231c9
warmist <warmist@gmail.com>
2023-03-13 14:38:12 +0200

    new idea: dna-based life sim

---
+++ projects/life_code2.lua
@@ -0,0 +1,26 @@
+--[[
+	idea:
+		* have agents
+		* having dna
+		* transcribe all of it (maybe starting from random locations)
+		* some codes generate signal strengths (e.g. move forward)
+		* some codes have preconditions (e.g. food available)
+		* some codes are more resistant to mutations (e.g. growth logic)
+		* biggest strength gets executed
+		* energy for most actions needed
+		* some way of breeding/mutations etc
+	must support:
+		* plants
+			- roots(???)
+			- trunk
+			- leaves (light -> energy)
+			- seeds/fruits
+		* worms
+			- grow
+			- move
+			- eat (non-armored -> energy)
+		* mushrooms
+			- grow
+			- spores
+			- organics -> soil + energy
+--]]
