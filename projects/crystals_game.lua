2644479f056ab84d23585cf74a474ab4d607fac5
warmist <warmist@gmail.com>
2023-04-18 10:24:03 +0300

    new project: crystal_game

---
+++ projects/crystals_game.lua
@@ -0,0 +1,20 @@
+--[[
+	game mechanic exploration based on crystals and crystal-based lifeform.
+
+	factorio like with some rts features. Maybe some procedural generated abilities and co.
+
+	Game phases:
+		* expansion
+			- grow from seed
+			- build infrastructure
+			- start the tech tree
+		* encounter
+			- enemy faction encounter
+			- battle over resources and survival
+		* domination
+			- fully conquer the field
+			- exploit resources and optimize the build
+		* spore creation
+			- final creation to next change the next start
+			- sort-of-idle game mechanic of reincarnation/meta progression
+--]]
