6975de6af2433e2ca1f8449b8a0352e841604643
warmist <warmist@gmail.com>
2023-04-18 10:57:38 +0300

    crystal_game more ideas

--- projects/crystals_game.lua
+++ projects/crystals_game.lua
@@ -3,6 +3,11 @@
 
 	factorio like with some rts features. Maybe some procedural generated abilities and co.
 
+	crystal like because this is rewareded:
+		- repeating structure
+		- symmetries
+		- scale symmetry? (hard to imagine how that would work)
+
 	Game phases:
 		* expansion
 			- grow from seed
@@ -17,4 +22,24 @@
 		* spore creation
 			- final creation to next change the next start
 			- sort-of-idle game mechanic of reincarnation/meta progression
+
+	Mechanics:
+		* resources
+			- energy-like - has no direct map location. "alive" tiles can store for no cost
+				- transport energy e.g. diagonally 
+			- matter-like - takes up space. storages have dedicated place for it. Movement is difficult?
+			- research-like ?
+			- supply-like (i.e like in rts supplies) - max unit support
+				- supply pixels e.g. two up, one right (thus enforce some sort of structure)
+		* interactions
+			- growth/assimilation
+			- mining
+			- energy creation
+			- "research"
+		* types of "constructions":
+			- seed/main crystal - big, has multiple abilities
+			- storage crystals
+			- support/supply
+			- mining?
+			- growht?
 --]]
