74b06e8655b3aeba4ccad23a6df96a0a5c0af2dc
warmist <warmist@gmail.com>
2023-05-29 11:21:12 +0300

    ecology4: no-bugs falling sand working now

--- projects/ecology4.lua
+++ projects/ecology4.lua
@@ -7,9 +7,9 @@
 
 require "common"
 local ffi=require "ffi"
-w=1024
-h=1024
-agent_count=100000
+w=256
+h=256
+agent_count=3000
 config=make_config({
     {"pause",false,type="bool"},
     },config)
@@ -22,13 +22,14 @@ end
 
 local kern_logic,kern_target,kern_move,kern_init,kern_output
 function remake_program()
-kern_logic,kern_target,kern_move,kern_init,kern_output=opencl.make_program(set_values(
+kern_logic,kern_target,kern_move,kern_init,kern_init_s,kern_output=opencl.make_program(set_values(
 [==[
 #pragma FILE cl_kernel
 #pragma LINE __LINE__
+#pragma OPENCL EXTENSION cl_khr_global_int32_base_atomics : enable
 #define W $w$
 #define H $h$
-#define AGENT_MAX 100000
+#define AGENT_MAX 3000
 #define TIME_STEP 0.005f
 
 int2 clamp_pos(int2 p)
@@ -61,11 +62,14 @@ uint pcg(uint v)
 int2 unpack_coord(int v)
 {
 	int2 ret;
-	ret.x=v&0xFFFF;
-	ret.y=(v>>16)&0xFFFF;
+	ret.x=abs(v&0xFFFF);
+	ret.y=abs((v>>16)&0xFFFF);
 	return ret;
 }
-__kernel void update_agent_logic(__global __read_only int4* input,__read_only image2d_t static_layer,__global __write_only int4* output)
+__kernel void update_agent_logic(__global __read_only int4* input,
+	__global __read_only int* static_layer,
+	__global __read_only int* dynamic_layer,
+	__global __write_only int4* output,int seed)
 {
 	int i=get_global_id(0);
 	int max=AGENT_MAX;
@@ -84,21 +88,62 @@ __kernel void update_agent_logic(__global __read_only int4* input,__read_only im
 			int2 target;
 			target=agent+d;
 			output[i].xy=agent;
+		#if 0
 			float4 col=read_imagef(static_layer,target);
 			float4 col1=read_imagef(static_layer,target+(int2)(1,0));
 			float4 col2=read_imagef(static_layer,target+(int2)(-1,0));
-			if(col.r+col.g+col.b==0)
+			if(dot(col,col)==0)
 				output[i].zw=target;
 			else
 			{
-				if(col1.r+col1.g+col1.b==0)
+				if(dot(col1,col1)==0)
 					output[i].zw=target+(int2)(1,0);
-				else if(col2.r+col2.g+col2.b==0)
+				else if(dot(col2,col2)==0)
 					output[i].zw=target+(int2)(-1,0);
 				else
 					output[i].zw=agent;
 			}
+		#else
+			uint r=pcg((uint)(seed+i));
+			int col=static_layer[target.x+target.y*W]+dynamic_layer[target.x+target.y*W];
+			int col1=static_layer[target.x+1+target.y*W]+dynamic_layer[target.x+1+target.y*W];
+			int col2=static_layer[target.x-1+target.y*W]+dynamic_layer[target.x-1+target.y*W];
+			if(col==0)
+				output[i].zw=target;
+			else
+			{
+				if(r%2==0)
+				{
+					if(col1==0)
+						output[i].zw=target+(int2)(1,0);
+					/*
+					else if(col2==0)
+						output[i].zw=target+(int2)(-1,0);
+					*/
+					else
+						output[i].zw=agent;
+				}
+				else
+				{
+					if(col2==0)
+						output[i].zw=target+(int2)(-1,0);
+					/*
+					else if(col1==0)
+						output[i].zw=target+(int2)(1,0);
+					*/
+					else
+						output[i].zw=agent;	
+				}
+			}
+		#endif
 		}
+	}
+}
+void increment(int2 pos,__global volatile int* movement_counts)
+{
+	if(pos.x>=0 && pos.x<W && pos.y>=0 && pos.y<H)
+	{
+		atomic_inc(movement_counts+(pos.x+pos.y*W));
 	}
 }
 __kernel void update_agent_targets(__global __read_only int4* input,__global volatile int* movement_counts)
@@ -107,14 +152,8 @@ __kernel void update_agent_targets(__global __read_only int4* input,__global vol
 	int max=AGENT_MAX;
 	if(i>=0 && i<max)
 	{
-		//int2 agent=input[i].xy;
-		int2 tagent=input[i].zw;
-		//if(agent.x>=0 && agent.x<W && agent.y>=0 && agent.y<H)
-		//	atomic_inc(movement_counts+(agent.x+agent.y*W));
-		if(tagent.x>=0 && tagent.x<W && tagent.y>=0 && tagent.y<H)
-		{
-			atomic_inc(movement_counts+(tagent.x+tagent.y*W));
-		}
+		increment(input[i].xy,movement_counts);
+		increment(input[i].zw,movement_counts);
 	}
 }
 __kernel void update_agent_move(__global __read_only int4* input,__global __read_only int* movement_counts,__global __write_only int4* output)
@@ -129,6 +168,7 @@ __kernel void update_agent_move(__global __read_only int4* input,__global __read
 			if(movement_counts[agent.x+agent.y*W]==1)
 			{
 				output[i].xy=agent;
+				output[i].zw=agent;
 			}
 			else
 				output[i]=input[i];
@@ -147,18 +187,34 @@ __kernel void init_agents(__global __write_only int4* output)
 	if(i>=0 && i<max)
 	{
 		#if 0
-		int2 trg=unpack_coord((int)pcg((uint)i));
-		output[i].x=trg.x % W;
-		output[i].y=H-(trg.y % (H/3));
+		int2 trg=unpack_coord((int)pcg((uint)i*7846));
+		output[i].x=abs(trg.x) % W;
+		output[i].y=H-(abs(trg.y) % (H/3));
 		#else
 		int r=(int)pcg((uint)i);
-		int j=i*10+r%10;
+		int j=i*10+abs(r)%10;
 		output[i].x=j % (W-2)+1;
 		output[i].y=H-(j / (W-2));
 		#endif
 	}
 }
-__kernel void output_to_texture(__global __read_only int4* input,__write_only image2d_t output_tex)
+__kernel void init_static(__global __write_only int4* output)
+{
+	int i=get_global_id(0);
+	int max=W*H;
+	if(i>=0 && i<max)
+	{
+		int x=i%W;
+		int y=i/W;
+		//float c=cos((float)i*5487697347779999578.15+4897778787.36)*0.5+0.5;
+		uint r=pcg(i^0x4a67fd);
+		if(r%7==0 && y<15)
+			output[i]=1;
+		else
+			output[i]=0;
+	}
+}
+__kernel void output_to_texture(__global __read_only int4* input,__global __write_only int* static_dynamic_layer,__write_only image2d_t output_tex)
 {
 	int i=get_global_id(0);
 	int max=AGENT_MAX;
@@ -167,9 +223,11 @@ __kernel void output_to_texture(__global __read_only int4* input,__write_only im
 		float4 col;
 		col.x=(float)i/(float)max;//cos((float)i*742.154+7745.0)*0.5+0.5;
 		col.y=cos((float)i*1141.154+774.0)*0.5+0.5;
-		col.z=cos((float)i*333.10+10.0)*0.5+0.5;
+		col.z=1;//cos((float)i*333.10+10.0)*0.5+0.5;
 		int2 pos=input[i].xy;
+		col.w=1;
 		write_imagef(output_tex,pos,col);
+		static_dynamic_layer[pos.x+pos.y*W]=i;
 	}
 }
 
@@ -184,6 +242,9 @@ buffers={
 	opencl.make_buffer(agent_count*16)
 }
 move_count_buffer=opencl.make_buffer(w*h*4)
+static_layer_buffer=opencl.make_buffer(w*h*4)
+sd_layer_buffer=opencl.make_buffer(w*h*4)
+
 texture=textures:Make()
 texture:use(0)
 texture:set(w,h,FLTA_PIX)
@@ -211,6 +272,11 @@ void main()
 function init_buffers(  )
 	kern_init:set(0,buffers[1])
 	kern_init:run(agent_count)
+	--[[
+	kern_init_s:set(0,static_layer_buffer)
+	kern_init_s:run(w*h)
+	--]]
+	static_layer_buffer:fill_i(w*h*4,1)
 end
 init_buffers()
 
@@ -230,42 +296,45 @@ function clear_counts(  )
 	move_count_buffer:fill_i(w*h*4,1)
 end
 function clear_display(  )
-	display_buffer:fill(w*h*16,4);
+	sd_layer_buffer:fill_i(w*h*4,1);
 end
+paused=false
 function update(  )
 	__no_redraw()
 	__clear()
-
-	clear_counts()
-	display_buffer:aquire()
-	kern_logic:set(0,buffers[1])
-	kern_logic:set(1,display_buffer)
-	kern_logic:set(2,buffers[2])
-	kern_logic:run(agent_count)
-	display_buffer:release()
-
-	kern_target:set(0,buffers[2])
-	kern_target:set(1,move_count_buffer)
-	kern_target:run(agent_count)
-
-	kern_move:set(0,buffers[2])
-	kern_move:set(1,move_count_buffer)
-	kern_move:set(2,buffers[1])
-	kern_move:run(agent_count)
-	--output
-	kern_output:set(0,buffers[2])
-	kern_output:set(1,display_buffer)
-
-
-	texture:use(1)
-	if not texture:render_to(w,h) then
-		error("failed to set framebuffer up")
+	local do_step=imgui.Button("Step")
+	if imgui.RadioButton("Paused",paused) then
+		paused=not paused
 	end
-	__clear()
-	__render_to_window()
-	display_buffer:aquire()
-	kern_output:run(agent_count)
-	display_buffer:release()
+	if do_step or not paused then
+		clear_counts()
+		kern_logic:set(0,buffers[1])
+		kern_logic:set(1,static_layer_buffer)
+		kern_logic:set(2,sd_layer_buffer)
+		kern_logic:set(3,buffers[2])
+		kern_logic:set(4,math.random(0,999999999))
+		kern_logic:run(agent_count)
+
+		kern_target:set(0,buffers[2])
+		kern_target:set(1,move_count_buffer)
+		kern_target:run(agent_count)
+
+		kern_move:set(0,buffers[2])
+		kern_move:set(1,move_count_buffer)
+		kern_move:set(2,buffers[1])
+		kern_move:run(agent_count)
+		--output
+	end
+		clear_display()
+		kern_output:set(0,buffers[2])
+		kern_output:set(1,sd_layer_buffer)
+		kern_output:set(2,display_buffer)
+
+
+		display_buffer:aquire()
+		kern_output:run(agent_count)
+		display_buffer:release()
+	
 	--gl draw
 	shader:use()
 	texture:use(1)
@@ -274,4 +343,14 @@ function update(  )
 	if imgui.Button("save") then
 		save_img()
 	end
+	-- [[
+	--if do_step then
+		texture:use(1)
+		if not texture:render_to(w,h) then
+			error("failed to set framebuffer up")
+		end
+		__clear()
+		__render_to_window()
+	--end
+	--]]
 end
