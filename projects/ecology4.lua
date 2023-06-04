6bd28d095ea0f2e6b210083ec8f75907be00d401
Warmist <warmist@gmail.com>
2023-06-04 08:56:52 +0300

    rebuild using structs

--- projects/ecology4.lua
+++ projects/ecology4.lua
@@ -7,9 +7,9 @@
 
 require "common"
 local ffi=require "ffi"
-w=1024
-h=1024
-agent_count=150000
+w=512
+h=512
+agent_count=15000
 config=make_config({
     {"pause",false,type="bool"},
     },config)
@@ -29,9 +29,15 @@ kern_logic,kern_target,kern_move,kern_init,kern_init_s,kern_output=opencl.make_p
 #pragma OPENCL EXTENSION cl_khr_global_int32_base_atomics : enable
 #define W $w$
 #define H $h$
-#define AGENT_MAX 150000
+#define AGENT_MAX 15000
 #define TIME_STEP 0.005f
-
+struct agent_state
+{
+	int pos;
+	int target;
+	int flags;
+	int id; //or sth...
+};
 int2 clamp_pos(int2 p)
 {
 #if 0
@@ -66,17 +72,22 @@ int2 unpack_coord(int v)
 	ret.y=abs((v>>16)&0xFFFF);
 	return ret;
 }
-__kernel void update_agent_logic(__global __read_only int4* input,
+int pack_coord(int2 v)
+{
+	int ret=(v.x&0xFFFF) | ((abs(v.y)&0xFFFF)<<16);
+	return ret;
+}
+__kernel void update_agent_logic(__global __read_only struct agent_state* input,
 	__global __read_only int* static_layer,
 	__global __read_only int* dynamic_layer,
-	__global __write_only int4* output,int seed)
+	__global __write_only struct agent_state* output,int seed)
 {
 	int i=get_global_id(0);
 	int max=AGENT_MAX;
 	if(i>=0 && i<max)
 	{
-		int2 agent=input[i].xy;
-		if(agent.x>0 && agent.x<W-1 && agent.y>0 && agent.y<H-1)
+		int2 pos=unpack_coord(input[i].pos);
+		if(pos.x>0 && pos.x<W-1 && pos.y>0 && pos.y<H-1)
 		{
 			/*
 			int2 d=unpack_coord(pcg((uint)i));
@@ -86,56 +97,74 @@ __kernel void update_agent_logic(__global __read_only int4* input,
 			*/
 			int2 d=(int2)(0,-1);
 			int2 target;
-			target=agent+d;
-			output[i].xy=agent;
+			target=pos+d;
+			output[i].pos=input[i].pos;
 		#if 0
 			float4 col=read_imagef(static_layer,target);
 			float4 col1=read_imagef(static_layer,target+(int2)(1,0));
 			float4 col2=read_imagef(static_layer,target+(int2)(-1,0));
 			if(dot(col,col)==0)
-				output[i].zw=target;
+				output[i].target=target;
 			else
 			{
 				if(dot(col1,col1)==0)
-					output[i].zw=target+(int2)(1,0);
+					output[i].target=target+(int2)(1,0);
 				else if(dot(col2,col2)==0)
-					output[i].zw=target+(int2)(-1,0);
+					output[i].target=target+(int2)(-1,0);
 				else
-					output[i].zw=agent;
+					output[i].target=agent;
 			}
 		#else
 			uint r=pcg((uint)(seed+i));
+
 			int col=static_layer[target.x+target.y*W]+dynamic_layer[target.x+target.y*W];
 			int col1=static_layer[target.x+1+target.y*W]+dynamic_layer[target.x+1+target.y*W];
 			int col2=static_layer[target.x-1+target.y*W]+dynamic_layer[target.x-1+target.y*W];
 
-			int col3=static_layer[agent.x+1+agent.y*W]+dynamic_layer[agent.x+1+agent.y*W];
-			int col4=static_layer[agent.x-1+agent.y*W]+dynamic_layer[agent.x-1+agent.y*W];
-
-			output[i].zw=agent;
+			int col3=static_layer[pos.x+1+pos.y*W]+dynamic_layer[pos.x+1+pos.y*W];
+			int col4=static_layer[pos.x-1+pos.y*W]+dynamic_layer[pos.x-1+pos.y*W];
+			int id=input[i].id;
+			output[i]=input[i];
+			output[i].target=pack_coord(pos);
 			if(col==0)
-				output[i].zw=target;
+				output[i].target=pack_coord(target);
 			else
 			{
-				if(r%4==0)
+				bool moved=false;
+				if(r%2)
 				{
 					if(col1==0)
-						output[i].zw=target+(int2)(1,0);
-				}
-				else if(r%4==1)
-				{
-					if(col2==0)
-						output[i].zw=target+(int2)(-1,0);
-				}
-				else if(r%4==2)
-				{
-					if(col3==0)
-						output[i].zw=agent+(int2)(1,0);
+					{
+						output[i].target=pack_coord(target+(int2)(1,0));
+						moved=true;
+					}
 				}
 				else
 				{
-					if(col4==0)
-						output[i].zw=agent+(int2)(-1,0);
+					if(col2==0)
+					{
+						output[i].target=pack_coord(target+(int2)(-1,0));
+						moved=true;
+					}
+				}
+				if(!moved && id==1)
+				{
+					if(r%2==0)
+					{
+						if(col3==0)
+						{
+							output[i].target=pack_coord(pos+(int2)(1,0));
+							moved=true;
+						}
+					}
+					else
+					{
+						if(col4==0)
+						{
+							output[i].target=pack_coord(pos+(int2)(-1,0));
+							moved=true;
+						}
+					}
 				}
 			}
 		#endif
@@ -149,29 +178,43 @@ void increment(int2 pos,__global volatile int* movement_counts)
 		atomic_inc(movement_counts+(pos.x+pos.y*W));
 	}
 }
-__kernel void update_agent_targets(__global __read_only int4* input,__global volatile int* movement_counts)
+__kernel void update_agent_targets(__global __read_only struct agent_state* input,__global volatile int* movement_counts)
 {
 	int i=get_global_id(0);
 	int max=AGENT_MAX;
 	if(i>=0 && i<max)
 	{
-		increment(input[i].xy,movement_counts);
-		increment(input[i].zw,movement_counts);
+		int2 pos=unpack_coord(input[i].pos);
+		int2 target=unpack_coord(input[i].target);
+		increment(pos,movement_counts);
+		increment(target,movement_counts);
 	}
 }
-__kernel void update_agent_move(__global __read_only int4* input,__global __read_only int* movement_counts,__global __write_only int4* output)
+__kernel void update_agent_move(__global __read_only struct agent_state* input,__global __read_only int* movement_counts,__global __write_only struct agent_state* output)
 {
+	/*
+		TODO:
+			* move +
+			* grow (move without clear at source)
+			* exchange (only static with dynamic)
+			* die?
+			* transforms might happen here too
+	*/
 	int i=get_global_id(0);
 	int max=AGENT_MAX;
 	if(i>=0 && i<max)
 	{
-		int2 agent=input[i].zw;
-		if(agent.x>=0 && agent.x<W && agent.y>=0 && agent.y<H)
+
+		int2 trg=unpack_coord(input[i].target);
+		if(trg.x>=0 && trg.x<W && trg.y>=0 && trg.y<H)
 		{
-			if(movement_counts[agent.x+agent.y*W]==1)
+			if(movement_counts[trg.x+trg.y*W]==1)
 			{
-				output[i].xy=agent;
-				output[i].zw=agent;
+				//output[i]=input[i];
+				output[i].pos=input[i].target;
+				output[i].target=input[i].target;
+				output[i].flags=input[i].flags;
+				output[i].id=input[i].id;
 			}
 			else
 				output[i]=input[i];
@@ -183,7 +226,7 @@ __kernel void update_agent_move(__global __read_only int4* input,__global __read
 	}
 }
 
-__kernel void init_agents(__global __write_only int4* output)
+__kernel void init_agents(__global __write_only struct agent_state* output)
 {
 	int i=get_global_id(0);
 	int max=AGENT_MAX;
@@ -197,8 +240,8 @@ __kernel void init_agents(__global __write_only int4* output)
 		int r=(int)pcg((uint)i);
 		int density=3;
 		int j=i*density+abs(r)%density;
-		output[i].x=j % (W-2)+1;
-		output[i].y=H-(j / (W-2));
+		output[i].pos=pack_coord( (int2)(j % (W-2)+1,H-(j / (W-2))) );
+		output[i].id=abs(r)%2;
 		#endif
 	}
 }
@@ -211,15 +254,18 @@ __kernel void init_static(__global __write_only int* output)
 		int x=i%W;
 		int y=i/W;
 		int dx=x-W/2;
-		int dy=y-H/2;
+		int dy=y;
 		int d=dx*dx+dy*dy;
 		//float c=cos((float)i*5487697347779999578.15+4897778787.36)*0.5+0.5;
 		uint r=pcg(i^0x4a67fd);
-		if(r%5==0 && y<600)
+		/*if(r%5==0 && y<600)
 			output[i]=1;
 		else if(r%4==0 && y<300 && d>100000)
 			output[i]=1;
 		else if(r%3==0 && y<300)
+			output[i]=1;
+		else*/
+		if(d<100000)
 			output[i]=1;
 		else
 			output[i]=0;
@@ -248,22 +294,22 @@ float3 spectral_zucconi6 (float w)
         bump3y(c1 * (x - x1), y1) +
         bump3y(c2 * (x - x2), y2) ;
 }
-__kernel void output_to_texture(__global __read_only int4* input,__global __write_only int* static_dynamic_layer,__write_only image2d_t output_tex)
+__kernel void output_to_texture(__global __read_only struct agent_state* input,__global __write_only int* static_dynamic_layer,__write_only image2d_t output_tex)
 {
 	int i=get_global_id(0);
 	int max=AGENT_MAX;
 	if(i>=0 && i<max)
 	{
-
+		float4 colors[2]={
+			(float4)(0.7,0.75,.8,1),
+			(float4)(0.8,0.2,0.3,1),
+		};
 		float4 col;
 		float v=(float)i/(float)max;
-		/*
-		col.x=v;//cos((float)i*742.154+7745.0)*0.5+0.5;
-		col.y=cos((float)i*1141.154+774.0)*0.5+0.5;
-		col.z=1;//cos((float)i*333.10+10.0)*0.5+0.5;
-		*/
-		col.xyz=spectral_zucconi6(v);
-		int2 pos=input[i].xy;
+		//col.xyz=spectral_zucconi6(v);
+		int id=clamp(input[i].id,0,1);
+		col=colors[id];
+		int2 pos=unpack_coord(input[i].pos);
 		col.w=1;
 		write_imagef(output_tex,pos,col);
 		static_dynamic_layer[pos.x+pos.y*W]=i;
