49d530ea645883ab511727c352c43a7d79be7966
warmist <warmist@gmail.com>
2024-09-30 13:01:08 +0300

    new idea

---
+++ projects/cellular_statistical.lua
@@ -0,0 +1,13 @@
+--[==[
+	cellular automata magic system or sth
+
+	2 main types with possible subtypes:
+		* "protons" - low numbers, define "atom" or molecule "centers"
+		* "electrons" - high numbers, maybe loosing them somehow teleporst em back?
+
+	Main idea:
+		* Define some conservation laws
+		* Generate rules
+		* Simulate 100(s) of interactions (i.e. sitting, moving, collision, etc)
+		* Statistically model some rules
+--]==]
