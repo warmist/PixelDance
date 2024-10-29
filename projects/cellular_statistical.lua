a6cd4aea074e9792fc2abc11beb3b84dc8b958f1
warmist <warmist@gmail.com>
2024-10-29 13:53:59 +0200

    misc stuff

--- projects/cellular_statistical.lua
+++ projects/cellular_statistical.lua
@@ -5,9 +5,24 @@
 		* "protons" - low numbers, define "atom" or molecule "centers"
 		* "electrons" - high numbers, maybe loosing them somehow teleporst em back?
 
+
+	Alt idea:
+		no protons, but figure out (??) or encode in rules some "stable arragement" of particles as "proton".
+		all non-stable parts are "gamma rays" or sth...
+		
 	Main idea:
 		* Define some conservation laws
 		* Generate rules
 		* Simulate 100(s) of interactions (i.e. sitting, moving, collision, etc)
 		* Statistically model some rules
+
+
+	Other idea:
+		* have complicated thingy
+		* remove complication
+		e.g. 
+			cells have 3 values
+			simplified to have 2
+
 --]==]
+
